e35c9ac0dda5050fe203193ce90307806ed5a349
lindstr_r <romeo.lindstrom06@outlook.com>
2025-01-15 22:26:22 +0200

    Added an ActivationStatus enum to represent all possible Windows license statuses.

--- WSI/include/wsi/systemapi.h
+++ WSI/include/wsi/systemapi.h
@@ -3,6 +3,17 @@
 #include <vector>
 #include "common.h"
 
+WSI_API enum ActivationStatus {
+	UNLICENSED,         // The system is not activated; no valid license is installed.
+	LICENSED,           // The system is fully activated and licensed.
+	OOB_GRACE,          // Out-of-Box Grace: The system is within the initial grace period after installation but not yet activated.
+	OOT_GRACE,          // Out-of-Tolerance Grace: Significant hardware changes detected; reactivation is required.
+	NON_GENUINE_GRACE,  // Non-Genuine Grace: The system is flagged as non-genuine due to an invalid or counterfeit license.
+	NOTIFICATION,       // Notification Mode: The system is functional but requires activation, with periodic reminders.
+	EXTENDED_GRACE,     // Extended Grace Period: The system has additional time to activate, typically for volume licenses.
+	UNAVAILABLE			// The system activation status is not available.
+};
+
 struct WSI_API ProcessorDetails {
 	std::wstring vendor;
 	int64_t architecture;
@@ -16,6 +27,7 @@ struct WSI_API OperatingSystemDetails {
 	std::wstring build_number;
 	std::wstring architecture;
 	std::wstring language;
+	ActivationStatus activation_status;
 };
 
 struct WSI_API DiskDetails {
