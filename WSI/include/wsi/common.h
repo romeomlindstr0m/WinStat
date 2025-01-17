5f393e334e3a8fea481685e0c5f0d776ef9e8407
lindstr_r <romeo.lindstrom06@outlook.com>
2025-01-17 11:06:17 +0200

    Revert "Added ERROR_MEMORY_STATUS_FAILED error code for memory status query failures."
    This reverts commit 2cfd3b132b649e268cee48015414727e5b0f8f90.

--- WSI/include/wsi/common.h
+++ WSI/include/wsi/common.h
@@ -37,8 +37,6 @@ constexpr int ERROR_QUERY_USER_NAME_FAILED = 1090; // User name query failed.
 constexpr int ERROR_QUERY_COMPUTER_NAME_FAILED = 1100; // Computer name query failed.
 constexpr int ERROR_UNEXPECTED_VARIANT_TYPE = 1110; // A WMI query returned a data type which was not expected
 
-constexpr int ERROR_MEMORY_STATUS_FAILED = 1120;
-
 // Warning codes are defined within a range of 10000-20000.
 constexpr int WARNING_WMI_CLASS_NOT_SET = 10000; // WMI class not set.
 constexpr int WARNING_WMI_PARTIAL_DATA = 10010; // A WMI query returned partial data, some properties may be missing.
