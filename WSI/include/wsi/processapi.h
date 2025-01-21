eaeebe5b910220a79e82f1bda2bf1ee4230b8583
lindstrom.romeo <64912856+romeomlindstr0m@users.noreply.github.com>
2025-01-21 05:22:10 +0200

    Enhancement: Enumerate processes (#7)
    * Rename functions to camelCase and add enumerateProcesses.
    
    * Rename functions to camelCase and implement enumerateProcesses.

--- WSI/include/wsi/processapi.h
+++ WSI/include/wsi/processapi.h
@@ -1,9 +1,11 @@
 #pragma once
 #include <cstdint>
 #include <string>
+#include <unordered_map>
 #include "wsi/common.h"
 
-int WSI_API GetProcessIdByName(std::wstring process_name, uint32_t& process_id, bool case_sensitive = true);
+int WSI_API getProcessIdByName(std::wstring process_name, uint32_t& process_id, bool case_sensitive = true);
+int WSI_API enumerateProcesses(std::unordered_map<uint32_t, std::wstring>& process_list);
 
-int WSI_API TerminateProcessById(uint32_t process_id);
-int WSI_API TerminateProcessByName(std::wstring process_name, bool case_sensitive = true);
+int WSI_API terminateProcessById(uint32_t process_id);
+int WSI_API terminateProcessByName(std::wstring process_name, bool case_sensitive = true);
