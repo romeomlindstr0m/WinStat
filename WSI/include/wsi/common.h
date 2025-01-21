5a7256f42ef829fd4bca3cd9ba5ed7e26f4daefd
lindstrom.romeo <64912856+romeomlindstr0m@users.noreply.github.com>
2025-01-21 02:18:07 +0200

    Functionality: Process API (#4)
    * Define IS_SUCCESS macro and add new processapi status codes.
    
    * Add processapi header file with API function declarations.
    
    * Implement processapi functions in source file.
    
    * Add processapi files to project solution.

--- WSI/include/wsi/common.h
+++ WSI/include/wsi/common.h
@@ -12,6 +12,9 @@ Usage:
 	Include this header where access to status codes are required.
 */
 
+// Define macro to check if a function or method succeeded without errors
+#define IS_SUCCESS(code) ((code) >= 0 && (code) <= 999)
+
 // Define preprocessor directive to export/import library components.
 #ifdef WSI_EXPORT
 #define WSI_API __declspec(dllexport)
@@ -37,8 +40,16 @@ constexpr int ERROR_QUERY_USER_NAME_FAILED = 1090; // User name query failed.
 constexpr int ERROR_QUERY_COMPUTER_NAME_FAILED = 1100; // Computer name query failed.
 constexpr int ERROR_UNEXPECTED_VARIANT_TYPE = 1110; // A WMI query returned a data type which was not expected
 
-constexpr int ERROR_MEMORY_STATUS_FAILED = 1120;
+constexpr int ERROR_MEMORY_STATUS_FAILED = 1120; // Something went wrong while querying memory information.
+
+constexpr int ERROR_PROCESS_SNAPSHOT_FAILED = 1130; // The system failed to create an snapshot of all running processes.
+constexpr int ERROR_PROCESS_FIRST_MISSING = 1140; // The first process entry is missing, this may indicate that the process list is empty.
+constexpr int ERROR_PROCESS_HANDLE_FAILED = 1150; // The system failed to create an handle to the specified process, this is usually because the calling application has insufficient permissions.
+constexpr int ERROR_PROCESS_TERMINATION_FAILED = 1160; // The system failed to terminate the specified process.
+constexpr int ERROR_PROCESS_ID_NOT_VALID = 1170; // The process is is not valid (the SYSTEM process cannot be terminated).
 
 // Warning codes are defined within a range of 10000-20000.
 constexpr int WARNING_WMI_CLASS_NOT_SET = 10000; // WMI class not set.
 constexpr int WARNING_WMI_PARTIAL_DATA = 10010; // A WMI query returned partial data, some properties may be missing.
+
+constexpr int WARNING_PROCESS_NOT_FOUND = 10020; // A process with the matching name was not found.
