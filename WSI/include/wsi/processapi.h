7297ae987d88ea3e056487a7e96815ec110f0d6e
romeomlindstr0m <romeo.lindstrom06@outlook.com>
2025-02-07 23:50:14 +0200

    Define ProcessInfo struct and update enumerateProcesses function signature in processapi.h.

--- WSI/include/wsi/processapi.h
+++ WSI/include/wsi/processapi.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <cstdint>
 #include <string>
-#include <unordered_map>
+#include <vector>
 #include "wsi/common.h"
 
 // Represents the priority level of a process.
@@ -17,12 +17,18 @@ WSI_API enum Priority {
     UNDEFINED = 8         // Placeholder for undefined priorities.
 };
 
+// This structure holds essential details about a running process, including its ID, parent process ID, name, and executable path.
+struct WSI_API ProcessInfo {
+    uint32_t id;                    // Process ID.
+    uint32_t parent_id;             // Parent Process ID.
+    std::wstring name;              // Process name.
+    std::wstring executable_path;   // Full path to the process executable.
+};
+
 // Retrieves the process ID of a process by its name.
 int WSI_API getProcessIdByName(std::wstring process_name, uint32_t& process_id, bool case_sensitive = true);
 // Enumerates all running processes on the system.
-int WSI_API enumerateProcesses(std::unordered_map<uint32_t, std::wstring>& process_list);
-// Retrieves the full executable path of a process by its ID.
-int WSI_API getExecutablePath(uint32_t process_id, std::wstring& executable_path);
+int WSI_API enumerateProcesses(std::vector<ProcessInfo>& process_list);
 // Terminates a process by its ID.
 int WSI_API terminateProcessById(uint32_t process_id);
 // Terminates a process by its name.
