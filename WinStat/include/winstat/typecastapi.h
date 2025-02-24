80a593c146aae0ee0b0f08f0b41976012c4d5c5b
romeomlindstr0m <romeo.lindstrom06@outlook.com>
2025-02-24 22:45:48 +0200

    Define utf16ToUtf8 function in typecastapi.h

--- WinStat/include/winstat/typecastapi.h
+++ WinStat/include/winstat/typecastapi.h
@@ -4,4 +4,6 @@
 
 namespace typecastapi {
 	int WINSTAT_API utf8ToUtf16(const std::string& utf8_string, std::wstring& utf16_string);
+	
+	int WINSTAT_API utf16ToUtf8(const std::wstring& utf16_string, std::string& utf8_string);
 }
