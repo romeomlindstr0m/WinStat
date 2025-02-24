e35c852280699c9c4a47b5082918c0e955b2ce45
romeomlindstr0m <romeo.lindstrom06@outlook.com>
2025-02-24 22:46:04 +0200

    Implement utf16ToUtf8 function in typecastapi.cpp

--- WinStat/src/typecastapi.cpp
+++ WinStat/src/typecastapi.cpp
@@ -40,3 +40,42 @@ int typecastapi::utf8ToUtf16(const std::string& utf8_string, std::wstring& utf16
 	delete[] utf16_buffer;
 	return SUCCESS;
 }
+
+int typecastapi::utf16ToUtf8(const std::wstring& utf16_string, std::string& utf8_string) {
+	int required_buffer_sz = WideCharToMultiByte(
+		CP_UTF8,
+		WC_ERR_INVALID_CHARS,
+		utf16_string.c_str(),
+		-1, NULL, 0, NULL, NULL);
+
+	if (required_buffer_sz == 0) {
+		DWORD last_error = GetLastError();
+
+		if (last_error == ERROR_NO_UNICODE_TRANSLATION) {
+			return ERROR_INVALID_CHARACTER_ENCOUNTERED;
+		}
+		else {
+			return ERROR_UTF16_BUFFER_FAILED;
+		}
+	}
+
+	char* utf8_buffer = new char[required_buffer_sz];
+
+	int conversion_result = WideCharToMultiByte(
+		CP_UTF8,
+		WC_ERR_INVALID_CHARS,
+		utf16_string.c_str(),
+		-1,
+		utf8_buffer,
+		required_buffer_sz,
+		NULL, NULL);
+
+	if (conversion_result == 0) {
+		delete[] utf8_buffer;
+		return ERROR_CONVERSION_FAILED;
+	}
+
+	utf8_string = std::string(utf8_buffer);
+	delete[] utf8_buffer;
+	return SUCCESS;
+}
