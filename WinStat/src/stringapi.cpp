4d3e1a03d826d95f0c467c4fb3ceac1ec9664134
lindstrom.romeo <64912856+romeomlindstr0m@users.noreply.github.com>
2025-02-27 10:42:39 +0200

    Functionality: String API (#27)
    * WIP: Initial implementation of stringapi.h functions (trim, transformCase)
    
    * Fix out-of-bounds access in stringapi::trim - stringapi.cpp

---
+++ WinStat/src/stringapi.cpp
@@ -0,0 +1,78 @@
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#include <Windows.h>
+#include <stdexcept>
+#include <wchar.h>
+#include "winstat/stringapi.h"
+
+std::wstring stringapi::trim(const std::wstring& string, stringapi::TrimMode trim_mode) {
+	if (string.empty()) { return L""; }
+
+	std::wstring string_buffer = string;
+
+	switch (trim_mode) {
+	case stringapi::TrimMode::LEFT:
+		while (string_buffer.length() != 0) {
+			if (string_buffer[0] == L' ') {
+				string_buffer.erase(string_buffer.begin());
+			}
+			else { break; }
+		}
+
+		break;
+	case stringapi::TrimMode::RIGHT:
+		while (string_buffer.length() != 0) {
+			if (string_buffer[string_buffer.length() - 1] == L' ') {
+				string_buffer.erase(string_buffer.end() - 1);
+			}
+			else { break; }
+		}
+
+		break;
+	case stringapi::TrimMode::BOTH:
+		while (string_buffer.length() != 0) {
+			if (string_buffer[0] == L' ') {
+				string_buffer.erase(string_buffer.begin());
+			}
+			else { break; }
+		}
+
+		while (string_buffer.length() != 0) {
+			if (string_buffer[string_buffer.length() - 1] == L' ') {
+				string_buffer.erase(string_buffer.end() - 1);
+			}
+			else { break; }
+		}
+
+		break;
+	default:
+		throw std::invalid_argument("Invalid TrimMode value.");
+		break;
+	}
+
+	return string_buffer;
+}
+
+std::wstring stringapi::transformCase(const std::wstring& string, stringapi::CaseMode case_mode) {
+	if (string.empty()) { return L""; }
+
+	wchar_t* string_buffer = new wchar_t[string.length() + 1];
+	wcscpy_s(
+		string_buffer,
+		static_cast<unsigned long long>(string.length() + 1),
+		string.c_str());
+
+	if (case_mode == stringapi::CaseMode::UPPER) {
+		CharUpperW(string_buffer);
+	}
+	else if (case_mode == stringapi::CaseMode::LOWER) {
+		CharLowerW(string_buffer);
+	}
+	else {
+		throw std::invalid_argument("Invalid CaseMode value.");
+	}
+
+	std::wstring result = std::wstring(string_buffer);
+	delete[] string_buffer;
+	return result;
+}
